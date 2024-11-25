1bc99b97b6d490ad39d2e53346c1df7eb7fe5c5f
thankyou0 <parthivva227@gmail.com>
2024-11-25 19:21:18 +0530

    return in bytext1

--- backend/algorithms/myFeed.js
+++ backend/algorithms/myFeed.js
@@ -68,9 +68,9 @@ const ByText1 = async (req, res) => {
     
     TextArray = (await getTextByCount(id));
     
-    return res.status(202).json({ message: "Internal Server No Error" });
+    // return res.status(202).json({ message: "Internal Server No Error" });
 
-    // let ArticlesByText1 = (await ScrapForFeed(TextArray.slice(0, 2)));
+    let ArticlesByText1 = (await ScrapForFeed(TextArray.slice(0, 2)));
 
 
     return res.status(202).json({ success: true, partialArticles: ArticlesByText1 });
